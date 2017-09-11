f625b6f86eacef91ae64aa683f6cabf7cd2230e1
Javier Jimenez, Michael Brooks and Riki Montgomery <michael@>
2017-09-11 15:47:18 -0700

    add the semi-final sinatra backend logic

--- app.rb
+++ app.rb
@@ -1,1 +1,18 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/triangle_type')
 
+get('/') do
+  erb(:input)
+end
+
+get('/output') do
+  @side1 = params.fetch("side1")
+  @side2 = params.fetch("side2")
+  @side3 = params.fetch("side3")
+  tri_type = Triangles.new(@side1, @side2, @side3)
+  @output = tri_type.triangle_type
+
+  erb(:output)
+end
